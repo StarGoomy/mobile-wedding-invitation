2580f9c1bf2c1348d5551ab735d7b09f502002a3
StarGoomy <tjsrb0716@gmail.com>
2025-07-13 16:39:47 +0900

    250713

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,9 +3,20 @@ import { getDatabase } from 'firebase/database';
 
 // TODO: https://firebase.google.com/docs/web/setup?hl=ko
 // 가이드에 따라 firebase config를 설정합니다.
-const firebaseConfig = {
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
 export const realtimeDb = getDatabase(firebaseApp);
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
